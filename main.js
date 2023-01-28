21c7d2ae43bcce33e37e28ac3a0d8a137f5accb8
onetrueaspirin <jelzinkos@gmail.com>
2023-01-28 18:29:19 -0300

    v2

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ let respuestaNombre = `Bien ${nombre}, podes empezar con los calculos`;
 alert(respuestaNombre)
 let numeroUno = parseInt(prompt("Ingrese el primer numero"));
 console.log(numeroUno);
-let operacion = prompt("ingrese la operacion deseada: %, +, -, *, /");
+let operacion = prompt(`ingrese la operacion deseada: %,
+ +,
+  -,
+   *,
+    /`);
 console.log(operacion);
 let numeroDos = parseInt(prompt("Ingrese el segundo numero"));
 console.log(numeroDos);
@@ -15,6 +19,19 @@ console.log(mensaje);
 if (resultado == '300') {
   alert("Ganaste un premio por encontrar easterEgg");
 
+}
+/* Numeros pares  */
+let numeroPar = 0;
+while (numeroPar < 1000) {
+  numeroPar++;
+  if (numeroPar % 2 == 0) console.log(numeroPar);
+}
+
+/* Numeros inpares */
+let numeroInp = 0;
+while (numeroInp < 1000) {
+  numeroInp++;
+  if (numeroInp % 2 == 1) console.log(numeroInp);
 }
 
 function calculadora(numero1, operacion, numero2) {
